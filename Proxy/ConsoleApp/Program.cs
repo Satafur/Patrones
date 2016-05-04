27ae5736a4b027a56a537f36b967dbc4e6305978
Esleider <sleider.a@live.com>
2016-05-04 00:02:52 -0500

    proyecto aplicando patron proxy
    un pequeño proyecto en C# usando visual studio en el cual se emplea el
    patrón de diseño proxy.

---
+++ Proxy/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Documento oDocumento = new Documento("Mi Documento");
+
+            try     // Necesario al usar Thread
+            {
+                // Pausamos la ejecución del programa durante 3 segundos
+                Thread.Sleep(3000);
+                // Simulamos que el usuario hace scroll
+                oDocumento.hacerScroll();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Ha ocurrido un error" + ex.Message);
+            }
+        }
+    }
+}
